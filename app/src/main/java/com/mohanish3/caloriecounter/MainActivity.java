8c6bc790184845284e9b5b63eb0ce575596acf25
mohanish3 <mohanish_mhatre@yahoo.com>
2018-08-06 16:37:34 +0530

    androidx dependencies added

---
+++ app/src/main/java/com/mohanish3/caloriecounter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mohanish3.caloriecounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
